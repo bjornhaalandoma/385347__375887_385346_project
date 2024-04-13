27bd935126c40875cc8a1e768fd01867c1f370d0
bjornhom <bjornhom@ntnu.no>
2024-04-13 13:33:53 +0200

    Logistic regression draft

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 from src.data import load_data
 from src.methods.dummy_methods import DummyClassifier
 from src.methods.logistic_regression import LogisticRegression
-from src.methods.linear_regression import LinearRegression 
+from src.methods.linear_regression import LinearRegression
 from src.methods.knn import KNN
 from src.utils import normalize_fn, append_bias_term, accuracy_fn, macrof1_fn, mse_fn
 import os
@@ -21,37 +21,33 @@ def main(args):
         args (Namespace): arguments that were parsed from the command line (see at the end 
                           of this file). Their value can be accessed as "args.argument".
     """
-    ## 1. First, we load our data and flatten the images into vectors
+    # 1. First, we load our data and flatten the images into vectors
 
-    ##EXTRACTED FEATURES DATASET
+    # EXTRACTED FEATURES DATASET
     if args.data_type == "features":
-        feature_data = np.load('features.npz',allow_pickle=True)
-        xtrain, xtest, ytrain, ytest, ctrain, ctest =feature_data['xtrain'],feature_data['xtest'],\
-        feature_data['ytrain'],feature_data['ytest'],feature_data['ctrain'],feature_data['ctest']
+        feature_data = np.load('features.npz', allow_pickle=True)
+        xtrain, xtest, ytrain, ytest, ctrain, ctest = feature_data['xtrain'], feature_data['xtest'], \
+            feature_data['ytrain'], feature_data['ytest'], feature_data['ctrain'], feature_data['ctest']
 
-    ##ORIGINAL IMAGE DATASET (MS2)
+    # ORIGINAL IMAGE DATASET (MS2)
     elif args.data_type == "original":
-        data_dir = os.path.join(args.data_path,'dog-small-64')
+        data_dir = os.path.join(args.data_path, 'dog-small-64')
         xtrain, xtest, ytrain, ytest, ctrain, ctest = load_data(data_dir)
 
-    ##TODO: ctrain and ctest are for regression task. (To be used for Linear Regression and KNN)
-    ##TODO: xtrain, xtest, ytrain, ytest are for classification task. (To be used for Logistic Regression and KNN)
+    # TODO: ctrain and ctest are for regression task. (To be used for Linear Regression and KNN)
+    # TODO: xtrain, xtest, ytrain, ytest are for classification task. (To be used for Logistic Regression and KNN)
 
-
-
-    ## 2. Then we must prepare it. This is were you can create a validation set,
+    # 2. Then we must prepare it. This is were you can create a validation set,
     #  normalize, add bias, etc.
 
     # Make a validation set (it can overwrite xtest, ytest)
     if not args.test:
-        ### WRITE YOUR CODE HERE
+        # WRITE YOUR CODE HERE
         pass
-    
-    ### WRITE YOUR CODE HERE to do any other data processing
 
-    
+    # WRITE YOUR CODE HERE to do any other data processing
 
-    ## 3. Initialize the method you want to use.
+    # 3. Initialize the method you want to use.
 
     # Use NN (FOR MS2!)
     if args.method == "nn":
@@ -61,11 +57,10 @@ def main(args):
     if args.method == "dummy_classifier":
         method_obj = DummyClassifier(arg1=1, arg2=2)
 
-    elif ...:  ### WRITE YOUR CODE HERE
-        pass
+    elif args.method == "logistic_regression":  # WRITE YOUR CODE HERE
+        method_obj = LogisticRegression(lr=0.001)
 
-
-    ## 4. Train and evaluate the method
+    # 4. Train and evaluate the method
 
     if args.task == "center_locating":
         # Fit parameters on training data
@@ -75,7 +70,7 @@ def main(args):
         train_pred = method_obj.predict(xtrain)
         preds = method_obj.predict(xtest)
 
-        ## Report results: performance on train and valid/test sets
+        # Report results: performance on train and valid/test sets
         train_loss = mse_fn(train_pred, ctrain)
         loss = mse_fn(preds, ctest)
 
@@ -89,7 +84,7 @@ def main(args):
         # Predict on unseen data
         preds = method_obj.predict(xtest)
 
-        ## Report results: performance on train and valid/test sets
+        # Report results: performance on train and valid/test sets
         acc = accuracy_fn(preds_train, ytrain)
         macrof1 = macrof1_fn(preds_train, ytrain)
         print(f"\nTrain set: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
@@ -98,31 +93,42 @@ def main(args):
         macrof1 = macrof1_fn(preds, ytest)
         print(f"Test set:  accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
     else:
-        raise Exception("Invalid choice of task! Only support center_locating and breed_identifying!")
+        raise Exception(
+            "Invalid choice of task! Only support center_locating and breed_identifying!")
 
-    ### WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
+    # WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
 
 
 if __name__ == '__main__':
     # Definition of the arguments that can be given through the command line (terminal).
     # If an argument is not given, it will take its default value as defined below.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', default="center_locating", type=str, help="center_locating / breed_identifying")
-    parser.add_argument('--method', default="dummy_classifier", type=str, help="dummy_classifier / knn / linear_regression/ logistic_regression / nn (MS2)")
-    parser.add_argument('--data_path', default="data", type=str, help="path to your dataset")
-    parser.add_argument('--data_type', default="features", type=str, help="features/original(MS2)")
-    parser.add_argument('--lmda', type=float, default=10, help="lambda of linear/ridge regression")
-    parser.add_argument('--K', type=int, default=1, help="number of neighboring datapoints used for knn")
-    parser.add_argument('--lr', type=float, default=1e-5, help="learning rate for methods with learning rate")
-    parser.add_argument('--max_iters', type=int, default=100, help="max iters for methods which are iterative")
-    parser.add_argument('--test', action="store_true", help="train on whole training data and evaluate on the test data, otherwise use a validation set")
-
+    parser.add_argument('--task', default="center_locating",
+                        type=str, help="center_locating / breed_identifying")
+    parser.add_argument('--method', default="dummy_classifier", type=str,
+                        help="dummy_classifier / knn / linear_regression/ logistic_regression / nn (MS2)")
+    parser.add_argument('--data_path', default="data",
+                        type=str, help="path to your dataset")
+    parser.add_argument('--data_type', default="features",
+                        type=str, help="features/original(MS2)")
+    parser.add_argument('--lmda', type=float, default=10,
+                        help="lambda of linear/ridge regression")
+    parser.add_argument('--K', type=int, default=1,
+                        help="number of neighboring datapoints used for knn")
+    parser.add_argument('--lr', type=float, default=1e-5,
+                        help="learning rate for methods with learning rate")
+    parser.add_argument('--max_iters', type=int, default=100,
+                        help="max iters for methods which are iterative")
+    parser.add_argument('--test', action="store_true",
+                        help="train on whole training data and evaluate on the test data, otherwise use a validation set")
 
     # Feel free to add more arguments here if you need!
 
     # MS2 arguments
-    parser.add_argument('--nn_type', default="cnn", help="which network to use, can be 'Transformer' or 'cnn'")
-    parser.add_argument('--nn_batch_size', type=int, default=64, help="batch size for NN training")
+    parser.add_argument('--nn_type', default="cnn",
+                        help="which network to use, can be 'Transformer' or 'cnn'")
+    parser.add_argument('--nn_batch_size', type=int,
+                        default=64, help="batch size for NN training")
 
     # "args" will keep in memory the arguments and their values,
     # which can be accessed as "args.data", for example.
--- src/methods/logistic_regression.py
+++ src/methods/logistic_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ..utils import get_n_classes, label_to_onehot, onehot_to_label
+from ..utils import get_n_classes, label_to_onehot, onehot_to_label, accuracy_fn
 
 
 class LogisticRegression(object):
@@ -20,7 +20,6 @@ class LogisticRegression(object):
         self.lr = lr
         self.max_iters = max_iters
 
-
     def fit(self, training_data, training_labels):
         """
         Trains the model, returns predicted labels for training data.
@@ -31,11 +30,11 @@ class LogisticRegression(object):
         Returns:
             pred_labels (array): target of shape (N,)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+
+        self.gradient_descent(training_data, training_labels)
+        pred_labels = self.logistic_regression_predict(
+            training_data, self.weights)
+
         return pred_labels
 
     def predict(self, test_data):
@@ -47,9 +46,68 @@ class LogisticRegression(object):
         Returns:
             pred_labels (array): labels of shape (N,)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        pred_labels = self.logistic_regression_predict(test_data, self.weights)
+
         return pred_labels
+
+    def f_softmax(self, data, W):
+        """
+        Args:
+            data (array): Input of shape (N,D)
+            W (array): Weights of shape (D,C) where C is the number of classes 
+        Returns:
+            array of shape (N,C)
+        """
+        return np.exp(data @ W) / np.sum(np.exp(data @ W), axis=1)[:, np.newaxis]
+
+    # TODO Remove? Not using currently, but can be used for visualization
+    def loss_function(self, data, labels, w):
+        """
+        Args:
+            data (array): Input of shape (N,D)
+            labels (array): Labels of shape (N,)
+            w (array): Weights of shape (D,C)
+        Returns:
+            float: Loss value 
+        """
+        return - np.sum(labels * np.log(self.f_softmax(data, w)))
+
+    def gradient_loss_function(self, data, labels, W):
+        """
+        Args:
+            data (array): Input of shape (N,D)
+            labels (_type_): Labels of shape (N,)
+            W (_type_): _description_
+        """
+
+        return data.T @ (self.f_softmax(data, W) - label_to_onehot(labels))
+
+    def logistic_regression_predict(self, data, W):
+        """
+        Args:
+            data (array): Dataset of shape (N,D). Contains N datasamples of dimension D
+            W (array): Weights of regression model of shape (D,C).
+        Return:
+            array of shape (N,): Labels predictions of data 
+        """
+        return np.argmax(self.f_softmax(data, W), axis=1)
+
+    def gradient_descent(self, data, labels):
+        """_summary_
+
+        Args:
+            data (_type_): _description_
+            labels (_type_): _description_
+        """
+        D = data.shape[1]
+        C = get_n_classes(labels)
+        weights = np.random.normal(0, 0.1, (D, C))
+
+        for it in range(self.max_iters):
+            predictions = self.logistic_regression_predict(data, weights)
+            if accuracy_fn(predictions, labels) >= 80:
+                break
+            weights -= self.lr * \
+                self.gradient_loss_function(data, labels, weights)
+
+        self.weights = weights
